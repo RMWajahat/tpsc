7243a1660b9f4299535f17ecf1a52f6e46007f3a
RMWajahat <rajawajahat2003@gmail.com>
2025-07-08 13:07:27 +0500

    Python Text Punctuation and Stopwords cleaner

--- text_cleanser/text_cleanser.py
+++ text_cleanser/text_cleanser.py
@@ -1,4 +1,5 @@
 import string
+import nltk
 from nltk.corpus import stopwords
 
 def remove_punc_and_stopwords(text_to_clean, output_format="text", lang="english"):
@@ -13,6 +14,13 @@ def remove_punc_and_stopwords(text_to_clean, output_format="text", lang="english
     Returns:
         list or text (str): Cleaned text.
     """
+
+    try:
+        stop_words = set(stopwords.words(lang))
+    except LookupError:
+        nltk.download('stopwords')
+        stop_words = set(stopwords.words(lang))
+
     if lang not in stopwords.fileids():
         return f"No support for language: {lang}"
 
@@ -20,6 +28,6 @@ def remove_punc_and_stopwords(text_to_clean, output_format="text", lang="english
     cleaned_punctuation_data = "".join([ch for ch in text_to_clean if ch not in string.punctuation])
     
     # Remove stopwords
-    cleaned_data = [word for word in cleaned_punctuation_data.split() if word.lower() not in stopwords.words(lang)]
+    cleaned_data = [word for word in cleaned_punctuation_data.split() if word.lower() not in stop_words]
 
     return " ".join(cleaned_data) if output_format == "text" else cleaned_data
